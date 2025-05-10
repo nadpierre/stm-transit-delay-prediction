f99f1511c261951c80f0fbeab02994148e757cb5
Nadine Pierre <nadine_pierre@hotmail.com>
2025-05-10 00:59:18 -0400

    residual analysis with best model

--- scripts/custom_functions.py
+++ scripts/custom_functions.py
@@ -4,12 +4,6 @@ import pandas as pd
 from pathlib import Path
 import requests
 
-DELAY_CLASS = {
-  0: 'Early',
-  1: 'OnTime',
-  2: 'Late'
-}
-
 LOCAL_TIMEZONE = 'Canada/Eastern'
 
 MTL_COORDS = {
@@ -21,6 +15,37 @@ SCHEDULE_RELATIONSHIP = {
   0: 'Scheduled',
   1: 'Skipped',
   2: 'NoData',
+}
+
+WEATHER_CODES = {
+  0: 'Clear sky',
+  1: 'Mainly clear',
+  2: 'Partly cloudy',
+  3: 'Overcast',
+  45: 'Fog',
+  48: 'Depositing rime fog',
+  51: 'Light drizzle',
+  53: 'Moderate drizzle',
+  55: 'Dense drizzle',
+  56: 'Light freezing drizzle',
+  57: 'Dense freezing drizzle',
+  61: 'Slight rain',
+  63: 'Moderate rain',
+  65: 'Heavy rain',
+  66: 'Light freezing rain',
+  67: 'Heavy frizzing rain',
+  71: 'Slight snow fall',
+  73: 'Moderate snow fall',
+  75: 'Heavy snow fall',
+  77: 'Snow grains',
+  80: 'Slight rain showers',
+  81: 'Moderate rain showers',
+  82: 'Violent rain showers',
+  85: 'Slight show showers',
+  86: 'Heavy show showers',
+  95: 'Slight or moderate thunderstorm',
+  96: 'Thunderstorm with slight hail',
+  99: 'Thunderstorm with heavy hail'
 }
 
 root_dir = Path(__file__).parent.parent.resolve()
